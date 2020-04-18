1dfb9908f7e270c3d59a8d6b1ec15a6ae79047e4
Rafael <faelleonan@gmail.com>
2020-04-17 23:13:58 -0300

    Implementando Material UI

--- src/components/containers/Header/index.js
+++ src/components/containers/Header/index.js
@@ -3,20 +3,21 @@ import './style.css';
 import logo from './imagens/logo.png';
 import lupa from './imagens/lupa.png';
 
+
 const Header = () => (
-    <header id="header">
-        <img src={ logo } className="logo" alt="Logo"/>
-        <div className="div-search">
-            <input type="text" name="search" placeholder="Pesquise algo"/>
-            <button type=""><img src={ lupa } alt="Lupa" id="lupa"/></button>
-        </div>
-        <div className="div-list">
-            <ul className="list-options">
-                <li><b>Entrar</b></li>
-                <li><b>Cadastrar</b></li>
-            </ul>
-        </div>
-    </header>
+<header id="header">
+    <img src={ logo } className="logo" alt="Logo"/>
+    <div className="div-search">
+        <input type="search" name="search" placeholder="Pesquise algo"/>
+        <button type=""><img src={ lupa } alt="Lupa" id="lupa"/></button>
+    </div>
+    <div className="div-list">
+        <ul className="list-options">
+            <li><b>Entrar</b></li>
+            <li><b>Cadastrar</b></li>
+        </ul>
+    </div>
+</header>
 );
 
 export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,132 @@
 import React, { Component } from 'react';
 import './style.css';
+import img from './img.jpg';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
 export default class Home extends Component{
     render(){
         return(
             <main className="content">
-                Home Page
+                <Container maxWidth="false">
+                    <ul className="list-card">
+                        <li>
+                            <Card className="card-wd">
+                                <CardActionArea>
+                                    <CardMedia
+                                    className="card-media"
+                                    image={img}
+                                    title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        React JS
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                    React é uma biblioteca JavaScript para construção de interfaces de usuário
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                    Compartilhar
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                    Ver mais
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card className="card-wd">
+                                <CardActionArea>
+                                    <CardMedia
+                                    className="card-media"
+                                    image={img}
+                                    title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        React JS
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                    React é uma biblioteca JavaScript para construção de interfaces de usuário
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                    Compartilhar
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                    Ver mais
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card className="card-wd">
+                                <CardActionArea>
+                                    <CardMedia
+                                    className="card-media"
+                                    image={img}
+                                    title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        React JS
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                    React é uma biblioteca JavaScript para construção de interfaces de usuário
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                    Compartilhar
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                    Ver mais
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card className="card-wd">
+                                <CardActionArea>
+                                    <CardMedia
+                                    className="card-media"
+                                    image={img}
+                                    title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        React JS
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                    React é uma biblioteca JavaScript para construção de interfaces de usuário
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                    Compartilhar
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                    Ver mais
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </li>
+                    </ul>
+                </Container>
             </main>
         );
     }
