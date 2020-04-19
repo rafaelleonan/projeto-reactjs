2f462e42b9240f58b24e3dd2f4110948d44bd829
Rafael <faelleonan@gmail.com>
2020-04-19 12:31:55 -0300

    Adicionando menu

--- src/components/containers/Header/index.js
+++ src/components/containers/Header/index.js
@@ -5,19 +5,33 @@ import lupa from '../../../static/icons/lupa.png';
 
 
 const Header = () => (
-<header id="header">
-    <img src={ logo } className="logo" alt="Logo"/>
-    <div className="div-search">
-        <input type="search" name="search" placeholder="Pesquise algo"/>
-        <button type=""><img src={ lupa } alt="Lupa" id="lupa"/></button>
-    </div>
-    <div className="div-list">
-        <ul className="list-options">
-            <li><span>Entrar</span></li>
-            <li><span>Cadastrar</span></li>
+<div className="cu">
+    <header id="header">
+        <img src={ logo } className="logo" alt="Logo"/>
+        <div className="div-search">
+            <input type="search" name="search" placeholder="Pesquise algo"/>
+            <button type=""><img src={ lupa } alt="Lupa" id="lupa"/></button>
+        </div>
+        <div className="div-list">
+            <ul className="list-options">
+                <li><span>Entrar</span></li>
+                <li><span>Cadastrar</span></li>
+            </ul>
+        </div>
+    </header>
+    <div className="menu">
+        <ul className="menu-list">
+            <li>Categorias</li>
+            <li>Categorias</li>
+            <li>Categorias</li>
+            <li>Categorias</li>
+            <li>Categorias</li>
+            <li>Categorias</li>
+            <li>Categorias</li>
+            <li>Categorias</li>
         </ul>
     </div>
-</header>
+</div>
 );
 
 export default Header;
